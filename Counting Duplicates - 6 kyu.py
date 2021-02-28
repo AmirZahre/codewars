b46d2fc7aa0acbde1759c3aeded3de864fd655d2
Amir <71795488+AmirZahre@users.noreply.github.com>
2021-02-28 09:08:07 -0700

    Update Counting Duplicates - 6 kyu.py

--- Counting Duplicates - 6 kyu.py
+++ Counting Duplicates - 6 kyu.py
@@ -1,15 +1,11 @@
 def duplicate_count(text):
 
     text_new = list(text.lower())
-
     x = 0
     for letter in text_new:
         if text_new.count(letter) > 1:
             text_new = [t for t in text_new if t != letter]
             x += 1
-
-
     return x
             
-
 print(duplicate_count("Indivisibilities"))
